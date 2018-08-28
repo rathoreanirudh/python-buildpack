e800ab20fe40c572a63f1a2c64ce8e4cd4d8f244
Anirudh Singh Rathore <singh16anirudh@gmail.com>
2018-08-28 11:51:17 +0530

    Update install_dependency.sh

--- scripts/install_dependency.sh
+++ scripts/install_dependency.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+export BoostInstallDir="/tmp/boost"
+mkdir -p $BoostInstallDir
 
 echo "Installing boost"
-sudo apt-get install libboost-all-dev
+cd $BoostInstallDir
+apt-get install libboost-all-dev
