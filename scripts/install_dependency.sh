e23b6f17687ef593ae672c37d1d570d147cd4852
Anirudh Singh Rathore <singh16anirudh@gmail.com>
2018-09-12 15:33:31 +0530

    Update install_dependency.sh

--- scripts/install_dependency.sh
+++ scripts/install_dependency.sh
@@ -16,7 +16,12 @@ curl -s -L --retry 15 --retry-delay 2 $URL -o /tmp/boost.tar.gz
 tar xzf /tmp/boost.tar.gz -C $BoostInstallDir
 cd $BoostInstallDir/boost_1_68_0/
 
+
+echo "Running bootstrap.sh"
 ./bootstrap.sh --with-libraries=python
+echo "Running ./b2"
 ./b2
+echo "Running b2 install"
 ./b2 install
+echo "done"
 rm /tmp/boost.tar.gz
